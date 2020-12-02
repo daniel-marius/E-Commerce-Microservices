71a809985db3231aedcd0ceafd296d15286b6d1b
danielmarius <danieladam01995@gmail.com>
2020-12-02 22:10:56 +0200

    New commit

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -2,13 +2,15 @@ import express, { Request, Response, Router } from 'express';
 import { requireAuth, NotFoundError, NotAuthorizedError  } from '@ticketsms/common';
 
 import { Order, OrderStatus } from '../models/order';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router: Router = express.Router();
 
 router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
   const { orderId } = req.params;
 
-  const order = await Order.findById(orderId);
+  const order = await Order.findById(orderId).populate('ticket');
 
   if (!order) {
     throw new NotFoundError();
@@ -22,10 +24,14 @@ router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Res
   await order.save();
 
   // Publishing an event saying this was cancelled
+  new OrderCancelledPublisher(natsWrapper.client).publish({
+    id: order.id,
+    ticket: {
+      id: order.ticket.id
+    }
+  });
 
   res.status(204).send(order);
 });
-
-it.todo('emits an order cancelled event');
 
 export { router as deleteOrderRouter };
